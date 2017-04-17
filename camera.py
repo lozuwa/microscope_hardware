7b410fd0e4aa790a0356ddd8317db62e34494969
Rodrigo Loza <lozuwaucb@gmail.com>
2017-04-17 18:44:33 -0400

    added new classes

--- camera.py
+++ camera.py
@@ -1,44 +1,39 @@
-import cv2
 import numpy as np
-import os, sys
+import os, sys, cv2
 import RPi.GPIO as gpio 
 
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
-try:
- gpio.setup(26, gpio.IN, gpio.PUD_UP) # y positionr 
-except:
- print('Failed GPIO initialization')
 
-try:
- cap = cv2.VideoCapture(0)
-except:
- try:
-  cap = cv2.VideoCapture(1)
- except:
-  try:
-   cap = cv2.VideoCapture(2)
-  except:
-   print('no camera')
-   sys.exit()
+class Camera():
+  def __init__(self):
+    # Initialize GPIO 
+    try:
+      gpio.setup(26, gpio.IN, gpio.PUD_UP) # y positionr 
+    except:
+      print('Failed GPIO initialization')
+    # Init camera 
+    try:
+      self.cap = cv2.VideoCapture(0)
+    except:
+      print('No camera')
+      sys.exit()
 
-def vision():
- c = 0
- while(1):
-  ret, frame = cap.read()
-  #frame = cv2.resize(frame, (720,640))
-  if gpio.input(26) == 0:
-   cv2.imwrite( 'images/' + str(c) + '.png', frame )
-   cv2.putText(frame, 'Foto tomada', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255, 2)
-   cv2.imshow('_', cv2.resize(frame,(720,640)))
-   cv2.waitKey(200)
-   c += 1
-  cv2.imshow('_', frame)#cv2.resize(frame,(720,640))) #cv2.pyrUp(frame, dstsize=(frame.shape[1]*1, frame.shape[0]*1)))
-  cv2.waitKey(1)
+  def vision():
+    c = 0
+    while(1):
+      ret, frame = self.cap.read()
+      #frame = cv2.resize(frame, (720,640))
+      if gpio.input(26) == 0:
+        cv2.imwrite( 'images/'+str(c)+'.png',frame )
+        cv2.putText( frame, 'Foto tomada', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255, 2 )
+        cv2.imshow('_', cv2.resize(frame,(720,640)))
+        cv2.waitKey(200)
+        c += 1
+      cv2.imshow('_', frame)
+      cv2.waitKey(1)
 
-def exit():
- cap.release()
- cv2.destroyAllWindows()
- sys.exit()
-
-vision()
+  def exit():
+    self.cap.release()
+    cv2.destroyAllWindows()
+    sys.exit()
--- z_positioner.py
+++ z_positioner.py
@@ -1,78 +1,74 @@
 import serial 
 import numpy as np
-import math
-import time 
+import math, time 
 import RPi.GPIO as GPIO 
 import os, sys 
 
+try:
+  ser = serial.Serial('/dev/ttyACM0', 115200, timeout=2)
+except:
+  try:
+    ser = serial.Serial('/dev/ttyACM1', 115200, timeout=2)
+  except:
+    print('Failed connection with device: ', ser)
+    sys.exit()
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(16, GPIO.IN, GPIO.PUD_UP) # z positioner
 
-try:
- ser = serial.Serial('/dev/ttyACM0', 115200, timeout=2)
-except:
- try:
-  ser = serial.Serial('/dev/ttyACM1', 115200, timeout=2)
- except:
-  try:
-   ser = serial.Serial('/dev/ttyACM2', 115200, timeout=2)
-  except:
-   print('Failed connection with device: ', ser)
-   sys.exit()
+class z_controller():
+  def __init__(self):
+    ser.write('x')
+    time.sleep(0.01)
 
-GPIO.setup(16, GPIO.IN, GPIO.PUD_UP) # z positioner 
+  def deactivate_control_loop(self):
+    ser.write('n')
+    time.sleep(0.01)
 
-def init():
- ser.write('x')
- time.sleep(0.01)
+  def activate_control_loop(self):
+    ser.write('y')
+    time.sleep(0.01)
 
-def deactivate_control_loop():
- ser.write('n')
- time.sleep(0.01)
+  def z_up(self):
+    ser.write('b')
+    wait()
 
-def activate_control_loop():
- ser.write('y')
- time.sleep(0.01)
+  def z_down(self):
+    if GPIO.input(16) == GPIO.LOW:
+      pass
+    else:
+      ser.write('z')
+      wait()
 
-def z_up():
- ser.write('b')
- wait()
+  def z_mid_up(self):
+    ser.write('B')
+    wait()
 
-def z_down():
- if GPIO.input(16) == GPIO.LOW:
-  pass
- else:
-  ser.write('z')
-  wait()
+  def z_mid_down(self):
+    if GPIO.input(16) == GPIO.LOW:
+      pass 
+    else:
+      ser.write('Z')
+      wait()
 
-def z_mid_up():
- ser.write('B')
- wait()
+  def wait(self):
+    counter = 0
+    while (ser.read() != 'o'):
+      counter += 1
+      if counter == 100:
+        print('broken point')
+        break
+      continue
 
-def z_mid_down():
- if GPIO.input(16) == GPIO.LOW:
-  pass 
- else:
-  ser.write('Z')
-  wait()
+  def z_reset(self):
+    while(GPIO.input(16) != GPIO.LOW):
+      self.z_down()
+    for i in range(25):
+      self.z_up()
+    #while(GPIO.input(18) != GPIO.LOW):
+    #  z_down()
 
-def wait():
- counter = 0
- while (ser.read() != 'o'):
-  counter += 1
-  if counter == 100:
-   print('broken point')
-   break
-  continue
-
-def z_reset():
- while(GPIO.input(16) != GPIO.LOW):
-  z_down()
- for i in range(25):
-  z_up()
- #while(GPIO.input(18) != GPIO.LOW):
- # z_down()
-
-def exit():
- ser.close()
- sys.exit()
+  def exit(self):
+    ser.close()
+    sys.exit()
