7e9fbf42f31c3a0bdf73bb117d14285310e2be72
lozuwa <lozuwaucb@gmail.com>
2017-08-08 13:13:20 -0400

    fixed serial com

--- MicroscopeProt6/autofocus.py
+++ MicroscopeProt6/autofocus.py
@@ -1,16 +1,16 @@
 # Author: Rodrigo Loza
-# Description: This script contains functions that interact with the microscope's hardware 
-# and the focus coefficients received from the sensor 
+# Description: This script contains functions that interact with the microscope's hardware
+# and the focus coefficients received from the sensor
 
 from interface import *
 
 class autofocus:
     def __init__(self, list = [(None, None)]):
-        self.positions = [each for each in list]
+        self.positions = [each[0] for each in list]
 	self.coefficients = [each[1] for each in list]
 
     def focus(self):
-        # Calculate the maximum value 
+        # Calculate the maximum value
 	max_ = max(self.coefficients)
 	pos = self.positions.index(max_)
         # Go back to the desired position
