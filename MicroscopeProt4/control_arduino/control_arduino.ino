776e7932847b1ab0a0c970f54ec7584a50eb708b
KhalilNallar <khalilnallar@gmail.com>
2017-09-01 18:25:57 -0400

    da

--- MicroscopeProt4/control_arduino/control_arduino.ino
+++ MicroscopeProt4/control_arduino/control_arduino.ino
@@ -4,17 +4,19 @@
 #define direccionZ 6
 #define stepsY 4
 #define direccionY 7
-#define enable 8
+#define enabley 8
+#define enablex A1
+#define enablez A0
 #define luz 12
 #define endX 11
 #define endY 10
 //#define endZ 9
-#define enablez A0
+
 
 String eje, pasos, direccion, timpo, brillo;
-byte brillo_actual=0;
-byte B_endStopX=0;
-byte B_endStopY=0;
+byte brillo_actual = 0;
+byte B_endStopX = 0;
+byte B_endStopY = 0;
 void setup() {
   pinMode(stepsX, OUTPUT);
   pinMode(direccionX, OUTPUT);
@@ -23,7 +25,8 @@ void setup() {
   pinMode(stepsZ, OUTPUT);
   pinMode(direccionZ, OUTPUT);
 
-  pinMode(enable, OUTPUT);
+  pinMode(enabley, OUTPUT);
+  pinMode(enablex, OUTPUT);
   pinMode(enablez, OUTPUT);
 
   pinMode(luz, OUTPUT);
@@ -32,7 +35,8 @@ void setup() {
   pinMode(endY, INPUT_PULLUP);
 
   digitalWrite(enablez, 1);
-  digitalWrite(enable , 1);
+  digitalWrite(enabley , 1);
+  digitalWrite(enablex , 1);
   digitalWrite(direccionZ, 0);
 
   Serial.begin(115200);
@@ -41,11 +45,13 @@ void setup() {
 
 void loop() {
   Serial.flush();
-  digitalWrite(enable , 1);
-  digitalWrite(direccionZ , 0);
-  digitalWrite(direccionY , 0);
-  digitalWrite(direccionX , 0);
-  endStop();
+  //endStop();
+  
+  //digitalWrite(enablez, 1);
+  //delay(300);
+  //digitalWrite(enablez, 0);
+  //delay(300);
+
   
   if (Serial.available() > 0) {
     String tx = Serial.readString();
@@ -77,15 +83,18 @@ void loop() {
       //Serial.print("moviendo eje z, " + pasos +" pasos, en direccion " + direccion + ",en " + timpo +" milisegundos por paso");
       z(pasos.toInt(), direccion.toInt(), timpo.toInt());
     }
-    else if(brillo.toInt()!=brillo_actual){
+    else if (brillo.toInt() != brillo_actual) {
       brillo_(brillo.toInt());
-      brillo_actual=brillo.toInt();
+      brillo_actual = brillo.toInt();
       //Serial.println("ok");
+    }
+    else {
+      return;
     }
   }
 }
-void brillo_(int brillo){
-  analogWrite(luz,brillo);
+void brillo_(int brillo) {
+  digitalWrite(luz, brillo);
 }
 void z(int pasos, int direccion, int timpo) {
   digitalWrite(direccionZ, direccion);
@@ -96,11 +105,12 @@ void z(int pasos, int direccion, int timpo) {
     digitalWrite(stepsZ, 0);
     delayMicroseconds(timpo);
   }
+  //delay(500);
   digitalWrite(enablez, 1);
-  Serial.write("o");
+  //  Serial.write("o");
 }
 void y(int pasos, int direccion, int timpo) {
-  digitalWrite(enable, 0);
+  digitalWrite(enabley, 0);
   digitalWrite(direccionY, direccion);
   for (int i = 0; i < pasos; i++) {
     digitalWrite(stepsY, 1);
@@ -108,10 +118,10 @@ void y(int pasos, int direccion, int timpo) {
     digitalWrite(stepsY, 0);
     delayMicroseconds(timpo);
   }
-  digitalWrite(enable , 1);
+  digitalWrite(enabley , 1);
 }
 void x(int pasos, int direccion, int timpo) {
-  digitalWrite(enable, 0);
+  digitalWrite(enablex, 0);
   digitalWrite(direccionX, direccion);
   for (int i = 0; i < pasos; i++) {
     digitalWrite(stepsX, 1);
@@ -119,25 +129,25 @@ void x(int pasos, int direccion, int timpo) {
     digitalWrite(stepsX, 0);
     delayMicroseconds(timpo);
   }
-  digitalWrite(enable , 1);
+  digitalWrite(enablex , 1);
 }
-void endStop(){
-  if(digitalRead(endX)==0 and B_endStopX==0){
-    B_endStopX=1;
+void endStop() {
+  if (digitalRead(endX) == 0 and B_endStopX == 0) {
+    B_endStopX = 1;
     Serial.println("x");
     delay(10);
   }
-  else if(digitalRead(endX)==1 and B_endStopX==1){
-    B_endStopX=0;
+  else if (digitalRead(endX) == 1 and B_endStopX == 1) {
+    B_endStopX = 0;
     delay(10);
   }
-  if(digitalRead(endY)==0 and B_endStopY==0){
-    B_endStopY=1;
+  if (digitalRead(endY) == 0 and B_endStopY == 0) {
+    B_endStopY = 1;
     Serial.println("y");
     delay(1);
   }
-  else if(digitalRead(endY)==1 and B_endStopY==1){
-    B_endStopY=0;
+  else if (digitalRead(endY) == 1 and B_endStopY == 1) {
+    B_endStopY = 0;
     delay(1);
   }
 }
