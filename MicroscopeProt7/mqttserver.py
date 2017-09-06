309710359ce45d5ff7ad5bcffacb4d4784789755
KhalilNallar <khalilnallar@gmail.com>
2017-09-06 17:42:13 -0400

    type: update description: new file:   MicroscopeProt7/autofocus.py new file:   MicroscopeProt7/autofocus.pyc modified:   MicroscopeProt7/mqttserver.py * Cleaned mqtt session and eliminated /connect topic. The new app no longer requires a state to enable connection. * The app's mqtt works with a background service. So the reconnection will be managed on second plane with a thread.   There is no need to mantain a second thread on the server to mantain connection.

--- MicroscopeProt7/mqttserver.py
+++ MicroscopeProt7/mqttserver.py
@@ -64,7 +64,6 @@ def timestamp():
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     # Microscope hardware
-    client.subscribe("/connect")
     client.subscribe("/zu")
     client.subscribe("/zd")
     client.subscribe("/led")
@@ -78,132 +77,121 @@ def on_connect(client, userdata, flags, rc):
 
 # Reply messages
 def on_message(client, userdata, msg):
-    global enable, stepsz, time_
+    global stepsz, time_
     global procZUp, procZDown
     global autofocusState, hardwareCode, countFrames
     global countPositions, saveAutofocusCoef
 
-    if msg.topic == "/connect":
+    if msg.topic == MOVEFIELDX_TOPIC:
         if int(msg.payload) == 1:
-            enable = True
-            print("server enabled")
-        elif int(msg.payload) == 2:
-            enable = False
-            print("server not enabled")
-    if enable == True:
-        if msg.topic == MOVEFIELDX_TOPIC:
-            if int(msg.payload) == 1:
-                axMov.moveFieldX(1)
-            elif int(msg.payload) == 0:
-                axMov.moveFieldX(0)
-            else:
-                pass
-        elif msg.topic == MOVEFIELDY_TOPIC:
-            if int(msg.payload) == 1:
-                axMov.moveFieldY(1)
-            elif int(msg.payload) == 0:
-                axMov.moveFieldY(0)
-            else:
-                pass
-        elif msg.topic == HOME_TOPIC:
-            axMov.home()
-        elif msg.topic == STEPS_TOPIC:
-            stepsz = float(msg.payload)*3
-            if stepsz <= 30:
-                stepsz = 30
-            else:
-                pass
-            print(msg.topic, stepsz)
-        elif msg.topic == LED_TOPIC:
-            if int(msg.payload) == 0 :
-                axMov.led.set_state(0)
-                ledState = axMov.led.get_state()
-                axMov.writeLed(ledState)
-            elif int(msg.payload) == 1:
-                axMov.led.set_state(1)
-                ledState = axMov.led.get_state()
-                axMov.writeLed(ledState)
-            else:
-                pass
-            print(msg.topic, msg.payload)
-        ##################################################################################
-        elif msg.topic == AUTOFOCUS_TOPIC:
-            print(msg.topic, msg.payload)
-            if msg.payload.decode("utf-8") == "start":
-                print("****************************Autofocus sequence****************************")
-                axMov.homeZ()
-                time.sleep(0.01)
-                autofocusState = True
-                countFrames = 0
-                publishMessage(AUTOFOCUS_TOPIC, "get")
-            elif msg.payload.decode("utf-8") == "stop":
-                print("***", saveAutofocusCoef)
-                aut = autofocus(saveAutofocusCoef)
-                goBack = aut.focus()
-                print("Go back ", goBack)
-                time.sleep(1)
-                for i in range(goBack):
-                    time.sleep(0.2)
-                    axMov.zResponse(350, 0, 250)
-                countPositions = 0
-                saveAutofocusCoef = []
-                print("****************************End autofocus sequence****************************")
-            else:
-                pass
-        ##################################################################################
-        elif msg.topic == VARIANCE_TOPIC:
-            if autofocusState:
-                if hardwareCode != "u":
-                    if countFrames < 1:
-                        print(msg.payload)
-                        saveAutofocusCoef.append((countPositions, float(msg.payload)))
-                        countFrames += 1
-                    else:
-                        hardwareCode = axMov.zResponse(250, 1, 250)
-                        publishMessage(AUTOFOCUS_TOPIC, "get")
-                        print("Hardware (mqtt) code: {}".format(hardwareCode))
-                        countPositions += 1
-                        countFrames = 0
+            axMov.moveFieldX(1)
+        elif int(msg.payload) == 0:
+            axMov.moveFieldX(0)
+        else:
+            pass
+    elif msg.topic == MOVEFIELDY_TOPIC:
+        if int(msg.payload) == 1:
+            axMov.moveFieldY(1)
+        elif int(msg.payload) == 0:
+            axMov.moveFieldY(0)
+        else:
+            pass
+    elif msg.topic == HOME_TOPIC:
+        axMov.home()
+    elif msg.topic == STEPS_TOPIC:
+        stepsz = float(msg.payload)*3
+        if stepsz <= 30:
+            stepsz = 30
+        else:
+            pass
+        print(msg.topic, stepsz)
+    elif msg.topic == LED_TOPIC:
+        if int(msg.payload) == 0 :
+            axMov.led.set_state(0)
+            ledState = axMov.led.get_state()
+            axMov.writeLed(ledState)
+        elif int(msg.payload) == 1:
+            axMov.led.set_state(1)
+            ledState = axMov.led.get_state()
+            axMov.writeLed(ledState)
+        else:
+            pass
+        print(msg.topic, msg.payload)
+    ##################################################################################
+    elif msg.topic == AUTOFOCUS_TOPIC:
+        print(msg.topic, msg.payload)
+        if msg.payload.decode("utf-8") == "start":
+            print("****************************Autofocus sequence****************************")
+            axMov.homeZ()
+            time.sleep(0.01)
+            autofocusState = True
+            countFrames = 0
+            publishMessage(AUTOFOCUS_TOPIC, "get")
+        elif msg.payload.decode("utf-8") == "stop":
+            print("***", saveAutofocusCoef)
+            aut = autofocus(saveAutofocusCoef)
+            goBack = aut.focus()
+            print("Go back ", goBack)
+            time.sleep(1)
+            for i in range(goBack):
+                time.sleep(0.2)
+                axMov.zResponse(350, 0, 250)
+            countPositions = 0
+            saveAutofocusCoef = []
+            print("****************************End autofocus sequence****************************")
+        else:
+            pass
+    ##################################################################################
+    elif msg.topic == VARIANCE_TOPIC:
+        if autofocusState:
+            if hardwareCode != "u":
+                if countFrames < 1:
+                    print(msg.payload)
+                    saveAutofocusCoef.append((countPositions, float(msg.payload)))
+                    countFrames += 1
                 else:
-                    autofocusState = False
-                    hardwareCode = "o"
-                    publishMessage(AUTOFOCUS_TOPIC, "stop")
-        ##################################################################################
-        elif msg.topic == ZUP_TOPIC:
-            if int(msg.payload) == 1:
+                    hardwareCode = axMov.zResponse(250, 1, 250)
+                    publishMessage(AUTOFOCUS_TOPIC, "get")
+                    print("Hardware (mqtt) code: {}".format(hardwareCode))
+                    countPositions += 1
+                    countFrames = 0
+            else:
+                autofocusState = False
+                hardwareCode = "o"
+                publishMessage(AUTOFOCUS_TOPIC, "stop")
+    ##################################################################################
+    elif msg.topic == ZUP_TOPIC:
+        if int(msg.payload) == 1:
+            print(msg.topic, int(msg.payload))
+            procZUp.start()
+        elif int(msg.payload) == 2:
+            try:
                 print(msg.topic, int(msg.payload))
-                procZUp.start()
-            elif int(msg.payload) == 2:
-                try:
-                    print(msg.topic, int(msg.payload))
-                    procZUp.terminate()
-                    procZUp = Process(target=zUp)
-                except:
-                    print("There was a problem with zu process")
-        elif msg.topic == ZDOWN_TOPIC:
-            if int(msg.payload) == 1:
+                procZUp.terminate()
+                procZUp = Process(target=zUp)
+            except:
+                print("There was a problem with zu process")
+    elif msg.topic == ZDOWN_TOPIC:
+        if int(msg.payload) == 1:
+            print(msg.topic, int(msg.payload))
+            procZDown.start()
+        elif int(msg.payload) == 2:
+            try:
                 print(msg.topic, int(msg.payload))
-                procZDown.start()
-            elif int(msg.payload) == 2:
-                try:
-                    print(msg.topic, int(msg.payload))
-                    procZDown.terminate()
-                    procZDown = Process(target=zDown)
-                except:
-                    print("There was a problem with zd process")
-    else:
-        print("server not enabled")
+                procZDown.terminate()
+                procZDown = Process(target=zDown)
+            except:
+                print("There was a problem with zd process")
 
 def publishMessage(topic, message, qos = 2):
     client.publish(topic, str(message), qos)
 
 if __name__ == "__main__":
     # Global variables
-    global stepsz, time_, enable
+    global stepsz, time_
     global procZUp, procZDown
     stepsz = 200
     time_ = 2000
-    enable = False
     procZUp = Process(target = zUp)
     procZDown = Process(target = zDown)
     # Autofocus variables
