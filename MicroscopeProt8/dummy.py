03c3d6160d532e79c9ea6e3eb2fbee55a184332c
lozuwa <lozuwaucb@gmail.com>
2017-09-29 13:27:38 -0400

    type: update desc: dummy

--- MicroscopeProt8/dummy.py
+++ MicroscopeProt8/dummy.py
@@ -33,30 +33,51 @@ client = mqtt.Client(clean_session = True)
 # Subscribe topics
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
+    client.subscribe("/automatic")
     client.subscribe("/variance")
     client.subscribe("/autofocus")
     
 # Reply messages
 def on_message(client, userdata, msg):
     global counter
-    global stateStop
     print(msg.topic, msg.payload)
+    if msg.topic == "/automatic":
+        if msg.payload.decode("utf-8") == "start":
+            print("Starting autofocus sequence ...")
+            time.sleep(1)
+            publishMessage("/autofocus", "get")
+            counter = 0
+        else:
+            pass
+    elif msg.topic == "/variance":
+        if counter <= 10:
+            print("Received message: ", msg.payload)
+            time.sleep(1)
+            print("Publishing get autofocus")
+            publishMessage("/autofocus", "get")
+            counter += 1
+        else:
+            print("Finished ", counter)
+    else:
+        pass
     
 def publishMessage(topic,
                     message,
-                    qos = 2):
+                    qos = 2,
+                    retain = False):
     """
     Publishes a mqtt message
     :param topic: input string that defines the target topic
     :param message: input string that denotes the content of the message
     :param qos: input int that defines the type of qos for the mqtt communication
+    :param retain: input boolean that defines if the message is set to retained
     """
     # Assert variables
     assert type(topic) == str, VARIABLE_IS_NOT_STR
     assert type(message) == str, VARIABLE_IS_NOT_STR
     assert type(qos) == int, VARIABLE_IS_NOT_INT
     # Publish message
-    client.publish(topic, message, qos = qos, retain = False)
+    client.publish(topic, message, qos = qos, retain = retain)
 
 if __name__ == "__main__":
     global counter
